61a6f157f8ff35d5288e271af6788f258ce61642
Toby Clarke <teeohbee@gmail.com>
2015-07-21 13:49:22 +0100

    Updated rspec unit test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,12 @@ require "docking_station"
 
 describe DockingStation do
 
-it {expect(respond_to?(:release_bike))}
+# describe DockingStation do
+#   it 'responds to release bike' do
+#     expect(subject).to respond_to :release_bike
+#   end
+# end
+
+it { is_expected.to respond_to :release_bike }
 
 end
