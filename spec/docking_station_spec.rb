e5f2a3210f081e87a1e1057bd4a09bf750de01b4
Toby Clarke <teeohbee@gmail.com>
2015-07-21 17:18:13 +0100

    Up to challenge 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require "docking_station"
+require "bike"
 
 describe DockingStation do
 
@@ -9,5 +10,15 @@ describe DockingStation do
 # end
 
 it { is_expected.to respond_to :release_bike }
+
+it "gets working bike" do
+  #expect(subject.release_bike).to be_instance_of Bike
+
+  bike = subject.release_bike
+  expect(bike).to be_working
+
+  #bike = subject.release_bike
+  #expect(bike).to :working?
+end
 
 end
